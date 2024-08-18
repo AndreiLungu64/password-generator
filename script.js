3191de94f86dc1d335e12e2808b7ddd8d50917c1
AndreiLungu64 <andreilungu777@yahoo.ro>
2024-08-18 16:09:42 +0300

    added character count and radio-checkbox relation features

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 const rangeInput = document.querySelector('input[type="range"]');
+const charLenLabel = document.querySelector("#char-len-value");
+const passOutput = document.querySelector(".pass-output");
+const radioContainer = document.querySelector(".settings");
+const radios = document.querySelectorAll('input[type="radio"]');
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+function initialise() {
+  charLenLabel.innerText = rangeInput.value;
+  radios[radios.length - 1].checked = true;
+  checkboxes.forEach((el) => (el.checked = true));
+}
+initialise();
 
 function updateRangeBackground() {
-    const value = (rangeInput.value - rangeInput.min) / (rangeInput.max - rangeInput.min) * 100;
-    rangeInput.style.background = `linear-gradient(to right, #6e6e6e 0%, #6e6e6e ${value}%, #17161E ${value}%, #17161E 100%)`;
+  const value = ((rangeInput.value - rangeInput.min) / (rangeInput.max - rangeInput.min)) * 100;
+  rangeInput.style.background = `linear-gradient(to right, #6e6e6e ${value}%, #17161E ${value}%)`;
+}
+updateRangeBackground();
+
+function generatePassword(len) {
+  const lettersCount = len;
+  const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const digits = "0123456789";
+  const symbols = "!@#$%^&*()_+-=[]{}|;:'\",.<>/?`~\\";
+  return "p".repeat(len);
 }
 
-rangeInput.addEventListener('input', updateRangeBackground);
-updateRangeBackground();
+rangeInput.addEventListener("input", function () {
+  updateRangeBackground();
+  const passLength = rangeInput.value;
+  charLenLabel.innerText = passLength;
+  passOutput.value = generatePassword(passLength);
+});
+
+radioContainer.addEventListener("click", function (e) {
+  const selectedValues = Array.from({ length: e.target.value }, (_, i) => i);
+  if (e.target.type === "radio") {
+    checkboxes.forEach((el) => (el.checked = false));
+    checkboxes.forEach((el, i) => (el.checked = selectedValues.includes(i)));
+  }
+});
