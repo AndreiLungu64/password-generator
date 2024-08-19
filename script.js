c88ebfb3da206ba763e05c8b24f3713fc67d6e00
AndreiLungu64 <andreilungu777@yahoo.ro>
2024-08-19 11:46:33 +0300

    Disabled excluded checkboxes from password formats; both radio buttons and checkboxes now dictate the character groups used

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ const checkboxContainer = document.querySelectorAll(".settings")[1];
 const radios = document.querySelectorAll('input[type="radio"]');
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 let paswordLength = 0;
-let passwordCharTpes = 0;
+let charGroupsToUse = 0;
+
+function updateRangeBackground() {
+  const value = ((rangeInput.value - rangeInput.min) / (rangeInput.max - rangeInput.min)) * 100;
+  rangeInput.style.background = `linear-gradient(to right, #6e6e6e ${value}%, #17161E ${value}%)`;
+}
+updateRangeBackground();
 
 function initialise() {
   charLenLabel.innerText = rangeInput.value;
@@ -16,23 +22,9 @@ function initialise() {
 }
 initialise();
 
-function updateRangeBackground() {
-  const value = ((rangeInput.value - rangeInput.min) / (rangeInput.max - rangeInput.min)) * 100;
-  rangeInput.style.background = `linear-gradient(to right, #6e6e6e ${value}%, #17161E ${value}%)`;
-}
-updateRangeBackground();
-
-radioContainer.addEventListener("click", function (e) {
-  const selectedValues = Array.from({ length: e.target.value }, (_, i) => i);
-  if (e.target.type === "radio") {
-    checkboxes.forEach((el) => (el.checked = false));
-    checkboxes.forEach((el, i) => (el.checked = selectedValues.includes(i)));
-  }
-});
-
 function generatePassword(len, charTypes) {
   let password = "";
-  const combinations = [
+  const groups = [
     ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
     ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
     ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
@@ -40,22 +32,48 @@ function generatePassword(len, charTypes) {
   ];
 
   let groupToUse = [];
-  combinations.forEach((group, i) => {
+  groups.forEach((group, i) => {
     if (i <= charTypes) groupToUse = groupToUse.concat(group);
   });
 
   const getRandomInt = (max) => Math.floor(Math.random() * max);
-  while (len > 0) {
-    password += groupToUse[getRandomInt(groupToUse.length - 1)];
-    len--;
-  }
+  while (len--) password += groupToUse[getRandomInt(groupToUse.length - 1)];
+
   return password;
 }
 
+function updatePassword() {
+    paswordLength = rangeInput.value;
+    charLenLabel.innerText = paswordLength;
+    passOutput.value = generatePassword(paswordLength, calcCharGroupsToUse());
+  }
+
+function calcCharGroupsToUse(){
+    return Array.from(checkboxes).filter((cb) => cb.checked).length - 1;
+}
+
+function disableCbox(charTypes) {
+    checkboxes.forEach((cb) => (cb.disabled = false));
+    checkboxes.forEach((cb, i) => {
+      if (i + 1 > charTypes) cb.disabled = true;
+    });
+  }
+
 rangeInput.addEventListener("input", function () {
   updateRangeBackground();
-  paswordLength = rangeInput.value;
-  passwordCharTpes = 3;
-  charLenLabel.innerText = paswordLength;
-  passOutput.value = generatePassword(paswordLength, passwordCharTpes);
+  updatePassword();
 });
+
+radioContainer.addEventListener("click", function (e) {
+  const selectedValues = Array.from({ length: e.target.value }, (_, i) => i);
+  if (e.target.type === "radio") {
+    checkboxes.forEach((el) => (el.checked = false));
+    checkboxes.forEach((el, i) => (el.checked = selectedValues.includes(i)));
+
+    charGroupsToUse = calcCharGroupsToUse();
+    disableCbox(charGroupsToUse);
+  }
+  //   updatePassword();
+});
+
+
