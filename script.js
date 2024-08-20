839a1812462e6584ddc2fd875613b2b1c9dea657
AndreiLungu64 <andreilungu777@yahoo.ro>
2024-08-20 15:17:28 +0300

    Password history items are now on the same line.

--- script.js
+++ script.js
@@ -124,8 +124,10 @@ checkboxContainer.addEventListener("click", function (e) {
 
 function addToHistoryList() {
   const passLi = `<div class = "list-item" >
-  <svg class = "history-coppy-btn" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" viewBox="0 0 256 256"><path d="M216,32H88a8,8,0,0,0-8,8V80H40a8,8,0,0,0-8,8V216a8,8,0,0,0,8,8H168a8,8,0,0,0,8-8V176h40a8,8,0,0,0,8-8V40A8,8,0,0,0,216,32ZM160,208H48V96H160Zm48-48H176V88a8,8,0,0,0-8-8H96V48H208Z"></path></svg> 
+  <svg class = "history-coppy-btn" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" viewBox="0 0 256 256"><path d="M216,32H88a8,8,0,0,0-8,8V80H40a8,8,0,0,0-8,8V216a8,8,0,0,0,8,8H168a8,8,0,0,0,8-8V176h40a8,8,0,0,0,8-8V40A8,8,0,0,0,216,32ZM160,208H48V96H160Zm48-48H176V88a8,8,0,0,0-8-8H96V48H208Z"></path></svg> 
   <li>${currentPassword}</li> </div>`;
+
+
   passList.insertAdjacentHTML("beforeend", passLi);
 
   const streLi = document.createElement("li");
