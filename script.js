59db22b1f5b84ad87633f0143a658f6d53c61ad6
AndreiLungu64 <andreilungu777@yahoo.ro>
2024-08-22 13:40:32 +0300

    Refactored button HTML and CSS structure for better readability.

--- script.js
+++ script.js
@@ -130,11 +130,11 @@ function addToHistoryList() {
   const now = new Intl.DateTimeFormat("en-US", options).format(date);
   const listLitem = `
   <div class="five-cells-grid">
-        <button class = "history-btn"><svg class="history-coppy-btn" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" viewBox="0 0 256 256"><path d="M216,32H88a8,8,0,0,0-8,8V80H40a8,8,0,0,0-8,8V216a8,8,0,0,0,8,8H168a8,8,0,0,0,8-8V176h40a8,8,0,0,0,8-8V40A8,8,0,0,0,216,32ZM160,208H48V96H160Zm48-48H176V88a8,8,0,0,0-8-8H96V48H208Z"></path></svg></button>
+        <button class = "history-btn history-coppy-btn"></button>
         <p class="list-sub-elem pass-sub-elem">${currentPassword}</p>
         <p class="list-sub-elem">${strengthBar.lastElementChild.textContent}</p>
         <p class="list-sub-elem">${now}</p>
-        <button class = "history-btn"><svg class = "history-delete-btn" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" viewBox="0 0 256 256"><path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"></path></svg></button>
+        <button class = "history-btn history-delete-btn"></button>
     </div>
     `;
 
@@ -152,13 +152,14 @@ coppyPassBtn.addEventListener("click", function () {
 });
 
 histList.addEventListener("click", function (e) {
+  console.log(e.target);
   if (e.target.classList.contains("history-coppy-btn")) {
-    navigator.clipboard.writeText(e.target.parentElement.nextElementSibling.textContent);
-    e.target.parentElement.classList.add("fade");
+    navigator.clipboard.writeText(e.target.nextElementSibling.textContent);
+    e.target.classList.add("fade");
     setTimeout(() => {
-      e.target.parentElement.classList.remove("fade");
-    }, "1500");
+      e.target.classList.remove("fade");
+    }, "600");
   } else if (e.target.classList.contains("history-delete-btn")) {
-    e.target.parentElement.parentElement.remove();
+    e.target.parentElement.remove();
   }
 });
